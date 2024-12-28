2552094f4009c2f16256a7fe13e98674f3a9f357
Fabrício Gonçalves Ferreira <fgoncalves@olie.ai>
2024-12-28 15:19:07 -0300

    feat: fonts from fontawesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ThemeProvider defaultTheme="dark" themes={['light', 'dark']}>
           {children}
         </ThemeProvider>
+        <Script src="https://kit.fontawesome.com/59948feead.js"></Script>
       </body>
     </html>
   );
