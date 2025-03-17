bfa98d01760d7d4791af9c8910b6e80433f980f6
Fabricio G. Ferreira <fabriciof481@gmail.com>
2025-03-16 22:52:24 -0300

    feat: finished jobs

--- app/_components/Jobs.tsx
+++ app/_components/Jobs.tsx
@@ -1,7 +1,6 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-
 import { tech } from "@/app/_components/Skills"
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import LaravelIcon from "@/components/icons/LaravelIcon"
 import VueIcon from "@/components/icons/VueIcon"
 import BootstrapIcon from "@/components/icons/BootstrapIcon"
@@ -13,7 +12,12 @@ import GitIcon from "@/components/icons/GitIcon"
 import CSSIcon from "@/components/icons/CSSIcon"
 import HTMLIcon from "@/components/icons/HTMLIcon"
 
+import Link from "next/link"
+import Image from "next/image"
+
 type job = {
+    logo: string,
+    link: string,
     enterprise: string,
     position: string,
     started_at: string,
@@ -24,14 +28,17 @@ type job = {
 
 const JOBS: Array<job> = [
     {
+        logo: "https://olie.ai/assets/logo-olie-landing-light-symbol-0e5bf777.png",
+        link: "https://olie.ai/",
         enterprise: "Olie",
         position: "Trainne",
         started_at: "28/08/2023",
         finished_at: "",
         paragraphs: [
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore aliquid atque iure harum. Fugiat explicabo hic accusamus voluptates, ipsum illum dolor. Velit ut eius mollitia ex voluptatibus eveniet reiciendis dignissimos?",
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore aliquid atque iure harum. Fugiat explicabo hic accusamus voluptates, ipsum illum dolor. Velit ut eius mollitia ex voluptatibus eveniet reiciendis dignissimos?",
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore aliquid atque iure harum. Fugiat explicabo hic accusamus voluptates, ipsum illum dolor. Velit ut eius mollitia ex voluptatibus eveniet reiciendis dignissimos?",
+            "A empresa Olie foi o meu primeiro emprego, e com ela pude compreender a grande diferença entre programar para o mercado e programar para estudos." ,
+            "Durante essa experiência, tive a oportunidade de compartilhar conhecimentos, mas, principalmente, de aprender com meus colegas de equipe. Eles me ensinaram diversos conceitos, práticas e abordagens que poderei aplicar em projetos futuros.",
+            "Ao longo da minha trajetória na Olie, recebi novas responsabilidades, que aceitei com entusiasmo e assumi com dedicação.",
+            "Realizávamos reuniões diárias com o objetivo de compartilhar os desafios encontrados durante o programa de trainee. Nessas reuniões, nosso tech leader nos orientava, ajudando-nos a encontrar soluções para os problemas enfrentados."
         ],
         technologies: [
             {name: "Laravel", icon: <LaravelIcon/>, progress: 0},
@@ -68,19 +75,22 @@ export default function Jobs(){
             {JOBS.map((job) => {
                 return (
                     <TabsContent key={buildId(job)} value={buildId(job)} className="flex flex-col gap-3 mt-0">
+                        <h4>
+                            {job.position} | {job.started_at} - {job.finished_at ? job.finished_at : "Presente"}
+                        </h4>
+
                         <div>
-                            <h4>{job.position}</h4>
-                            <span>{job.started_at} - {job.finished_at ? job.finished_at : "Presente"}</span>
-                        </div>
-                        <div>
+                            <Link href={job.link} target="_blank" className="float-left m-4">
+                                <Image src={job.logo} alt="Logo da olie" width={200} height={100}/>
+                            </Link>
                             {job.paragraphs.map((paragraph) => {
-                                return <p key={paragraph}>{paragraph}</p>
+                                return <p key={paragraph} className="pb-3">{paragraph}</p>
                             })}
                         </div>
                         <div className="flex flex-wrap gap-3">
                             {job.technologies.map((technologie) => {
                                 return (
-                                    <div key={technologie.name} className="border rounded-lg flex gap-1 px-2 py-1 text-lg font-bold">
+                                    <div key={technologie.name} className="border rounded-lg flex gap-2 px-2 py-1 text-lg font-bold">
                                         <div className="w-6">
                                             {technologie.icon}
                                         </div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["olie.ai"], // Adiciona o domínio externo para permitir a exibição da imagem
+  },
 };
 
 export default nextConfig;
