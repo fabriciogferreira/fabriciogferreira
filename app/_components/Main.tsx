9ba1508fa967e835ea89e65923941aa9f82dc943
Fabricio G. Ferreira <fabriciof481@gmail.com>
2025-03-16 23:01:46 -0300

    feat: first word in uppercase

--- app/_components/Main.tsx
+++ app/_components/Main.tsx
@@ -10,7 +10,7 @@ export default function Main() {
                         {section.showTitle && 
                             <div className="flex items-center mb-5">
                                 <i className={`fa-solid fa-${section.icon} fa-fw mr-2 fa-xl`}></i>
-                                <h1 className="text-4xl text-start">{section.title}</h1>
+                                <h1 className="text-4xl text-start">{section.title.charAt(0).toUpperCase() + section.title.slice(1)}</h1>
                             </div>
                         }
                         <section.component />
