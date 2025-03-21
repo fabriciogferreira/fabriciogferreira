3ffb6b02910d54e743281e0d33ca9cac1bf0cff7
Fabrício Gonçalves Ferreira <fgoncalves@olie.ai>
2025-03-21 18:49:37 -0300

    feat: finish downloads

--- app/_components/Donwloads.tsx
+++ app/_components/Donwloads.tsx
@@ -1,19 +1,18 @@
 import { buttonVariants } from '@/components/ui/button';
 
 import { files } from '@/types/files';
+import Link from 'next/link';
 
 export default function Downloads(){
     return (
         <div className='flex flex-wrap gap-2 justify-start'>
-            {Object.values(files).map((file) => {
-                return (
-                    <a key={file.name} className={buttonVariants({ variant: "secondary" })} download href={file.path} target="_blank" rel="noopener noreferrer">
-                        <i className="fa-solid fa-download"></i> |
-                        <i className={`fa-solid fa-${file.icon}`}></i> 
-                        {file.name}
-                    </a>
-                )
-            })}
+            {Object.values(files).map((file) =>
+                <Link key={file.name} className={buttonVariants({ variant: "secondary" })} download href={file.path} target="_blank" rel="noopener noreferrer">
+                    <i className="fa-solid fa-download"></i> |
+                    <i className={`fa-solid fa-${file.icon}`}></i> 
+                    {file.name}
+                </Link>
+            )}
         </div>
     )
 }
