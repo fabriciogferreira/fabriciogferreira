8c059e63365ab1f2ffa6f0f01bbf062100ed2df6
Fabrício Gonçalves Ferreira <fabriciof481@gmail.com>
2025-03-22 11:36:45 -0300

    feat: finish projects

--- app/_components/Projects.tsx
+++ app/_components/Projects.tsx
@@ -8,136 +8,104 @@ import {
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
 
-import { tech } from "@/app/_components/Skills"
+import Link from "next/link"
+import { buttonVariants } from "@/components/ui/button"
+import { projects } from "@/types/projects"
+import { Badge } from "@/components/ui/badge"
+import { Star } from 'lucide-react';
+import { GitFork } from 'lucide-react';
+import { ExternalLink } from 'lucide-react';
+import Image from "next/image"
 
-type project = {
-    name: string,
-    images: Array<string>,
-    description: string,
-    stars: number,
-    forks: number,
-    lastUpdate: string,
-    source: string,
-    site: string,
-    tags: Array<string>,
-    technologies: Array<tech>,
-}
-
-// const PROJECTS: Array<project> = [
-//     {
-//         name: ""
-//     }
-// ]
-
-export default function Projects(){
+export default function Projects(){    
     return (
         <div className="flex flex-wrap gap-2">
-            TODO: FILTRO
-            <Card>
-                <CardHeader>
-                    <Carousel 
-                        opts={{
-                            align: "start",
-                            loop: true,
-                        }}
-                    >
-                        <CarouselContent className="-ml-4">
-                            <CarouselItem className="basis-1/2">
-                                imagens
-                            </CarouselItem>
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-                    <CardTitle>Nome</CardTitle>
-                    <CardDescription>publicado em</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>description</p>
-                    <p>tecnologias</p>
-                    <p>tags: backend, frontend, fullstack, ia e etc</p>
-                </CardContent>
-                <CardFooter>
-                    <span>visit: caso esteja online</span>
-                    <span>source: caso seja público</span>
-                    <span>stars</span>
-                    <span>Last update</span>
-                    <span>forks</span>
-                    <span>likes, comentários</span>
-                </CardFooter>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <Carousel 
-                        opts={{
-                            align: "start",
-                            loop: true,
-                        }}
-                    >
-                        <CarouselContent className="-ml-4">
-                            <CarouselItem className="basis-1/2">
-                                imagens
-                            </CarouselItem>
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-                    <CardTitle>Nome</CardTitle>
-                    <CardDescription>publicado em</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>description</p>
-                    <p>tecnologias</p>
-                    <p>tags: backend, frontend, fullstack, ia e etc</p>
-                </CardContent>
-                <CardFooter>
-                    <span>visit: caso esteja online</span>
-                    <span>source: caso seja público</span>
-                    <span>stars</span>
-                    <span>Last update</span>
-                    <span>forks</span>
-                    <span>likes, comentários</span>
-                </CardFooter>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <Carousel 
-                        opts={{
-                            align: "start",
-                            loop: true,
-                        }}
-                    >
-                        <CarouselContent className="-ml-4">
-                            <CarouselItem className="basis-1/2">
-                                imagens
-                            </CarouselItem>
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-                    <CardTitle>Nome</CardTitle>
-                    <CardDescription>publicado em</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>description</p>
-                    <p>tecnologias</p>
-                    <p>tags: backend, frontend, fullstack, ia e etc</p>
-                </CardContent>
-                <CardFooter>
-                    <span>visit: caso esteja online</span>
-                    <span>source: caso seja público</span>
-                    <span>stars</span>
-                    <span>Last update</span>
-                    <span>forks</span>
-                    <span>likes, comentários</span>
-                </CardFooter>
-            </Card>
+            {/* TODO: FILTRO */}
+            {projects.map((project) => 
+                <Card key={project.name}>
+                    <CardHeader>
+                        {project.images.length > 0 && 
+                            <Carousel 
+                                opts={{
+                                    align: "center",
+                                    loop: true,
+                                    axis: "y",
+                                    dragFree: false,
+                                }}
+                                className="mx-[3rem]"
+                            >
+                                <CarouselContent>
+                                    {project.images.map(image => 
+                                        <CarouselItem key={image} className="basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5 flex flex-col border rounded-lg gap-4 mx-4 pl-0 p-1 h-40 relative">
+                                            <Image
+                                                objectFit="contain"
+                                                src={image}
+                                                alt={image}
+                                                fill
+                                            />
+                                        </CarouselItem>
+                                    )}
+                                </CarouselContent>
+                                <CarouselPrevious />
+                                <CarouselNext />
+                            </Carousel>
+                        }
+                        <CardTitle>{project.name}</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-col gap-2">
+                        <p>{project.description}</p>
+                        <div className="flex gap-2 flex-wrap">
+                            {project.technologies.map(tech => 
+                                <Badge key={tech.name} className="p-1.5 rounded">
+                                    <tech.icon/>
+                                    <span className="ms-1">{tech.name}</span>
+                                </Badge>
+                            )}
+                        </div>
+                        <div className="flex flex-wrap gap-2 ">
+                            {project.tags.map(tag => 
+                                <Badge key={tag}>{tag}</Badge>)
+                            }
+                        </div>
+                    </CardContent>
+                    <CardFooter className="flex flex-col items-stretch">
+                        <div className="flex justify-between mb-2">
+                            <Link href={project.site} target="_blank" className={buttonVariants({variant: "secondary"})}>
+                                Visitar
+                            </Link>
+                            <Link href={project.site} target="_blank" className={buttonVariants({variant: "secondary"})}>
+                                Testar como admin
+                            </Link>
+                        </div>
+                        {project.repositories.map(repository => 
+                            <div key={repository.link} className="flex justify-between">
+                                <Link href={repository.link} className="flex items-center gap-1">
+                                    <ExternalLink size={15}/>
+                                    {repository.name}
+                                </Link>
+                                <div className="flex gap-2">
+                                    <span className="flex items-center gap-1">
+                                        <Star size={15}/>
+                                        {repository.stars}
+                                    </span>
+                                    <span className="flex items-center gap-1">
+                                        <GitFork size={15}/>
+                                        {repository.forks}
+                                    </span>
+                                    <span>
+                                        {repository.updateAt}
+                                    </span>
+                                </div>
+                            </div>
+                        )}
+                    </CardFooter>
+                </Card>
+            )}
         </div>
     )
 }
---
+++ types/projects.ts
@@ -0,0 +1,58 @@
+import { tTechnologies, technologies } from "@/types/technology"
+import { tTags } from "@/types/tag"
+import { tRepositories } from "@/types/repository"
+
+export type tProject = {
+    name: string,
+    images: Array<string>,
+    description: string,
+    site: string,
+    tags: tTags,
+    technologies: tTechnologies,
+    repositories: tRepositories
+}
+
+export type tProjects = Array<tProject>
+
+export const projects: tProjects = [
+    {
+        name: "1% Melhor a Cada Dia",
+        images: [],
+        description: "Uma iniciativa totalmente pessoal, afim de resolver os problemas que eu tinha de organizas meu afazeres, gastos, treino, estudos e outros. Ele permite que você gerencie sua vida dentro dele, dividido em diversas partes para que você tenha independência entre as partes.",
+        repositories: [],
+        site: "https://opbed.com",
+        tags: ["Fullstack", "Backend", "Frontend"],
+        technologies: [
+            technologies.CSS,
+            technologies.Git,
+            technologies.PostgreSQL,
+            technologies.Laravel,
+            technologies.Javascript,
+            technologies.PHP,
+            technologies.HTML,
+            technologies.GitHub,
+            technologies.Docker
+        ]
+    },
+    {
+        name: "Adriano Veículos",
+        images: [],
+        description: "O adriano veículos jales é um site desenvolvido por mim e um amigo, ele foi desenvolvido no começo da minha carreira na programação, 5 meses após eu entrar no mercado. nele é possível que você veja os carros cadastrados pelo dono da concessinária.",
+        repositories: [],
+        site: "https://adrianoveiculosjales.com.br/",
+        tags: ["Fullstack", "Backend", "Frontend"],
+        technologies: [
+            technologies.Docker,
+            technologies.Javascript,
+            technologies.Laravel,
+            technologies.PHP,
+            technologies.PostgreSQL,
+            technologies.HTML,
+            technologies.Git,
+            technologies.CSS,
+            technologies.Bootstrap,
+            technologies.GitHub,
+        ]
+    }
+]
+
